4d826e1a0bb5e3a4c9eaa30cac1df19871abb149
RohanKhodade <rohankhodade883@gmail.com>
2026-07-19 11:52:44 +0530

    added API gateway, cors from each service removed because request will now pass from api gateway as aservice, cors config in api gateway because its the service which will talk to client( browser/react client), routing in gateway added and tested

---
+++ apiGateway/src/main/java/com/ecom/apiGateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.ecom.apiGateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+		System.out.println("ShopStack | Ecommerce Api Gateway Started at port: 8079");
+	}
+
+}
