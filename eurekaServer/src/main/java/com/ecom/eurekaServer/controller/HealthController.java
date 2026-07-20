ebccb956cedd4a3c7467058d3a2ed6357e6bd80d
RohanKhodade <rohankhodade883@gmail.com>
2026-07-20 17:02:00 +0530

    health check controller for eureka server

---
+++ eurekaServer/src/main/java/com/ecom/eurekaServer/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.ecom.eurekaServer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String health() {
+        return "Eureka Server Application is running at port 8080...";
+    }
+}
